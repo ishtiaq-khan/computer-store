46b49b7e82a6dde93f20b8561c959c1637711f54
ishtiaq <khan98x@gmail.com>
2023-04-28 04:34:09 -0400

    resetpassword page updated

--- src/pages/Laptops.js
+++ src/pages/Laptops.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import BreadCrumb from '../components/BreadCrumb'
-// import Meta from '../components/Meta'
+import Meta from '../components/Meta'
 
 const Laptops = () => {
   return (
     <>
-    {/* <Meta title={"Laptops"}/> */}
+    <Meta title={"Laptops"}/>
     <BreadCrumb title={"Laptop"}/>
     <div>Laptops</div>
     </>
